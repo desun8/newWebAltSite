21bb6d6e5251d34de1279389936a1613b0ae8958
Evgeniy Pchelinov <epchelinov@gmail.com>
2020-08-10 17:27:22 +1000

    добавил курсор для typewrite анимации

--- src/js/components/typewriteAnimation.js
+++ src/js/components/typewriteAnimation.js
@@ -8,6 +8,8 @@ export default class TypewriteAnimation {
 
     this.isComplete = false;
 
+    this.cursor = null;
+
     this.tween = null;
 
     this.init();
@@ -15,10 +17,17 @@ export default class TypewriteAnimation {
 
   wrapChar(char) {
     const elm = document.createElement('span');
-    elm.innerHTML = char;
+    if (char) {
+      elm.innerHTML = char;
+    }
     elm.classList.add(this.className);
 
     return elm;
+  }
+
+  trimText(text) {
+    const regex = /\s{2,}/gi;
+    return text.replace(regex, ' ');
   }
 
   insertChars(text) {
@@ -29,12 +38,24 @@ export default class TypewriteAnimation {
 
   replaceNode(nodes) {
     for (const node of nodes) {
-      if (node.nodeName !== '#text') {
-        // для ссылок в тексте
-        node?.childNodes[0]?.nodeName === '#text'
-        && node.childNodes[0].replaceWith(...this.insertChars(node.innerText));
-      } else {
-        node.replaceWith(...this.insertChars(node.nodeValue));
+      if (node.className !== this.className) {
+        if (node.nodeName !== '#text') {
+          // для ссылок в тексте
+          if (node.className === 'link') {
+            const iconWrap = node.querySelector('.link__icon');
+            const icon = iconWrap.querySelector('svg');
+
+            node?.childNodes[0]?.nodeName === '#text'
+            && node.childNodes[0].replaceWith(...this.insertChars(node.innerText));
+
+            const wrap = this.wrapChar();
+            wrap.classList.add(this.className);
+            wrap.appendChild(icon);
+            iconWrap.appendChild(wrap);
+          }
+        } else {
+          node.replaceWith(...this.insertChars(this.trimText(node.nodeValue)));
+        }
       }
     }
   }
@@ -43,17 +64,58 @@ export default class TypewriteAnimation {
     this.chars = this.elm.querySelectorAll(`.${this.className}`);
   }
 
+  createCursor() {
+    const elm = document.createElement('span');
+    elm.style.cssText = 'position: absolute; top: 4px; left: 0; width: 5px; height: 15px; border: 1px solid #000; margin-left: 20px';
+
+    this.elm.style.position = 'relative';
+    this.elm.appendChild(elm);
+    this.cursor = elm;
+  }
+
+  animationCursor(pos = { x: 0, y: 0 }) {
+    const { x, y } = pos;
+    gsap.to(this.cursor, { x, y, duration: 0.05, ease: 'none' });
+  }
+
   animation(chars) {
+    let { elm, animationCursor } = this;
+
+    animationCursor = animationCursor.bind(this);
+    const rectElm = elm.getBoundingClientRect();
+    let posY = 0;
+
     return gsap.from(chars, {
       alpha: 0,
       duration: 0.01,
-      delay: (i) => i * 0.05,
       ease: 'none',
+      stagger: {
+        each: 0.1,
+        onStart() {
+          const char = this.targets()[0];
+          const rectChar = char.getBoundingClientRect();
+
+          let y = rectChar.y - rectElm.y;
+
+          if (y !== 0) {
+            posY = y - posY > 10 ? y : posY;
+            console.log(posY);
+          }
+
+          animationCursor({
+            x: rectChar.x - rectElm.x,
+            y: posY
+          });
+        },
+      },
       onStart: () => {
         this.elm.style.opacity = '1';
         this.isComplete = false;
       },
-      onComplete: () => this.isComplete = true
+      onComplete: () => {
+        posY = 0;
+        this.isComplete = true;
+      }
     });
   }
 
@@ -71,6 +133,7 @@ export default class TypewriteAnimation {
 
     this.replaceNode(this.elm.childNodes);
     this.getCharElms();
+    this.createCursor();
     this.tween = this.animation(this.chars).pause();
   }
 }
