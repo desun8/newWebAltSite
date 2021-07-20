cc4f3272178e5e58d5d469f8784e5296dee8d523
Evgeniy <epchelinov@gmail.com>
2021-07-20 22:47:19 +1000

    завершил переезд на windicss

---
+++ windi.config.js
@@ -0,0 +1,52 @@
+const colors = require("windicss/colors");
+const typography = require("windicss/plugin/typography");
+
+module.exports = {
+  extract: {
+    include: ["./**/*.html"],
+  },
+  safelist: ["prose", "prose-sm", "m-auto"],
+  darkMode: "class",
+  plugins: [typography],
+  theme: {
+    letterSpacing: {
+      wide: "0.015em",
+    },
+
+    boxShadow: {
+      collapsing:
+        "1px 0 0 0 #131313, 0 1px 0 0 #131313, 1px 1px 0 0 #131313, 1px 0 0 0 #131313 inset, 0 1px 0 0 #131313 inset",
+    },
+
+    letterSpacing: {
+      wide: ".015em",
+    },
+
+    extend: {
+      colors: {
+        teal: colors.teal,
+        black: "#131313",
+        "black-opacity-50": "rgba(0, 0, 0, 0.5)",
+        "black-second": "#262626",
+        "black-second-opacity-10": "rgba(38, 38, 38, 0.1)",
+        gray: "#dfdddf",
+        orange: "#ff5000",
+        "orange-opacity-50": "rgba(255, 80, 0, 0.5)",
+      },
+
+      gridTemplateColumns: {
+        subscribe: "260px auto",
+        fillAuto: "1fr auto",
+        "auto-fill": "repeat(auto-fill, minmax(190px, 1fr))",
+        autoFill: "repeat(auto-fill, minmax(170px, 1fr))",
+      },
+
+      spacing: {
+        19: "4.75rem",
+        22: "5.5rem",
+        "3/2": "150%",
+        "5/4": "125%",
+      },
+    },
+  },
+};
