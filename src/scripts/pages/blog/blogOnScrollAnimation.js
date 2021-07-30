a1bb13b406cb1a38d7000b09a263c3271fd358a8
Evgeniy <epchelinov@gmail.com>
2021-07-30 17:34:41 +1000

    feat(блог): убран боковой декоративный элемент

--- src/scripts/pages/blog/blogOnScrollAnimation.js
+++ src/scripts/pages/blog/blogOnScrollAnimation.js
@@ -1,12 +1,10 @@
 import { shuffle } from 'lodash';
-
 import { gsap } from 'gsap';
 
 const DURATION = 0.2;
 
 const topTextElms = shuffle(Array.from(document.querySelectorAll('.blog-deco-top__item')));
 const topCrossElm = document.querySelector('.blog-deco-top__cross');
-const sideElm = document.querySelector('.blog-deco-side');
 
 const topAnimation = (pos) => {
   gsap.to([topTextElms, topCrossElm], {
@@ -19,14 +17,8 @@ const topAnimation = (pos) => {
   });
 };
 
-const sideAnimation = (pos) => {
-  gsap.killTweensOf(sideElm);
-  gsap.to(sideElm, { y: `${pos * -0.1}vh`, duration: DURATION, ease: 'none' });
-};
-
 const blogOnScrollAnimation = (pos) => {
   topTextElms && topTextElms.length > 0 && topCrossElm && topAnimation(pos);
-  sideElm && pos < 1000 && sideAnimation(pos);
 };
 
 export default blogOnScrollAnimation;
