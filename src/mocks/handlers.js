28a9d935af5f74b34a9bd5e88385087f385a5b0a
Evgeniy <epchelinov@gmail.com>
2021-08-02 11:53:44 +1000

    chore(msw): добавлен api для статей блога

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,26 +1,37 @@
-import { rest } from 'msw';
+import { rest } from "msw";
 
-// import jsonData from './data.json';
+import jsonBlog from "./blog.json";
 
 export const handlers = [
-  rest.post('/subscribe', (req, res, ctx) => {
+  rest.post("/subscribe", (req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.body(JSON.stringify({
-        message: 'OK'
+        message: "OK"
       }))
     );
   }),
 
-  rest.post('/form', (req, res, ctx) => {
+  rest.post("/form", (req, res, ctx) => {
     return res(
       // Respond with a 200 status code
       ctx.status(200),
       ctx.body(
         JSON.stringify({
-          message: 'OK',
+          message: "OK",
         })
       )
     );
   }),
+
+  rest.post("/api/blog", (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          results: jsonBlog,
+          success: true,
+        })
+      );
+    }
+  ),
 ];
--- src/scripts/blog.js
+++ src/scripts/blog.js
@@ -2,5 +2,14 @@ import initRoot from "./root";
 import { initApp } from "./pages/blog/vue";
 import "@/styles/blog.scss";
 
+function prepare() {
+  if (process.env.NODE_ENV === "development") {
+    return import("../mocks/browser.js")
+      .then((module) => module.worker.start());
+  }
+
+  return Promise.resolve();
+}
+
 initRoot();
-initApp();
+prepare().then(() => initApp());
