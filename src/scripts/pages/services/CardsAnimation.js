3b2bf32752bb79557b7cf50283d2e55796cf3110
Evgeniy <epchelinov@gmail.com>
2021-08-11 12:30:34 +1000

    fix(страница услуги): поправилась верстка

--- src/scripts/pages/services/CardsAnimation.js
+++ src/scripts/pages/services/CardsAnimation.js
@@ -1,20 +1,20 @@
 class CardsAnimation {
   constructor() {
-    this.titles = document.querySelectorAll('.service-card__title svg');
-    this.icons = document.querySelectorAll('.service-card__link svg');
+    this.titles = document.querySelectorAll(".service-card__title svg");
+    this.icons = document.querySelectorAll(".service-card__link svg");
   }
 
   animation(el, isForward) {
     if (isForward) {
-      el.classList.add('is-animation');
+      el.classList.add("is-animation");
     } else {
-      el.classList.remove('is-animation');
+      el.classList.remove("is-animation");
     }
   }
 
   intersectionObserver() {
     const isVisible = (entries) => {
-      entries.forEach(entry => {
+      entries.forEach((entry) => {
         const { target, isIntersecting } = entry;
 
         this.animation(target, isIntersecting);
@@ -27,11 +27,11 @@ class CardsAnimation {
   init() {
     this.observer = this.intersectionObserver();
 
-    this.titles.forEach(title => {
+    this.titles.forEach((title) => {
       this.observer.observe(title);
     });
 
-    this.icons.forEach(icon => {
+    this.icons.forEach((icon) => {
       this.observer.observe(icon);
     });
   }
--- src/scripts/services.js
+++ src/scripts/services.js
@@ -1,9 +1,20 @@
-// import './pages/blog/vue';
-import CardsAnimation from './pages/services/CardsAnimation';
-import APP from './app/APP';
-import "./root";
-import '@/styles/services.scss';
+import { gsap } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import initRoot from "./root";
+import CardsAnimation from "./pages/services/CardsAnimation";
+import APP from "./app/APP";
+import "@/styles/services.scss";
 
-if (!APP.isDesktop) {
+initRoot();
+
+if (APP.isDesktop) {
+  const elm = document.querySelector(".service-banner__icon");
+  ScrollTrigger.create({
+    target: elm,
+    animation: gsap.to(elm, { rotate: 10 }),
+    start: "center top",
+    scrub: true,
+  });
+} else {
   new CardsAnimation().init();
 }
