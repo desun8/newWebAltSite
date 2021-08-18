5f32b0b0619d42c48e7497b880c6c05ce2e789f9
Evgeniy <epchelinov@gmail.com>
2021-08-18 19:51:30 +1000

    feat(страница типовая): верстка + функционал + анимации

--- src/scripts/innerPage.ts
+++ src/scripts/innerPage.ts
@@ -1,7 +1,63 @@
+import { gsap } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 import initRoot from "./root";
 import "@/styles/inner.scss";
+import { mediaQueryEvent } from "./utils/mediaQueryEvent";
+import { initSubscribeBlock } from "./components/subscribeBlock";
+import submitForm from "./components/subscribe-banner/submitForm";
 
 initRoot();
 
-// TODO: MOBILE -> init subscribe form submit
-// TODO: DESKTOP -> init subscribe form submit
+const initSubscribeBlockElms = () => {
+  const pageContainer = document.querySelector<HTMLElement>(".page-container")!;
+  const subscribeElmSm = document.querySelector<HTMLElement>(
+    ".subscribe-block:not(.subscribe-block--transparent)"
+  );
+  const subscribeElmXl = document.querySelector<HTMLElement>(
+    ".subscribe-block.subscribe-block--transparent"
+  );
+
+  mediaQueryEvent(
+    () => {
+      if (subscribeElmSm) initSubscribeBlock(subscribeElmSm);
+    },
+    () => {
+      if (subscribeElmXl) {
+        const is2XLScreen = window.matchMedia("(min-width: 96em)").matches;
+        if (is2XLScreen) {
+          initSubscribeBlock(subscribeElmXl);
+
+          ScrollTrigger.create({
+            trigger: subscribeElmXl,
+            start: "top center",
+            end: () =>
+              `top -${pageContainer.offsetHeight - window.screen.height * 3}`,
+            pin: true,
+            pinSpacing: false,
+          });
+        }
+      }
+    }
+  );
+};
+
+const initSubscribeBanner = () => {
+  const subscribeBanner = document.querySelector(".subscribe-banner-block");
+
+  if (subscribeBanner) {
+    const bannerElm = subscribeBanner.querySelector(".js-banner")!;
+    const formElm = subscribeBanner.querySelector("form")!;
+
+    submitForm(formElm);
+
+    ScrollTrigger.create({
+      trigger: bannerElm,
+      animation: gsap.to(bannerElm, { x: 0, duration: 0.35 }),
+      start: "center 80%",
+      once: true,
+    });
+  }
+};
+
+initSubscribeBlockElms();
+initSubscribeBanner();
