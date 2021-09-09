620cc97a057975ed202cac371580fed05a92e55f
Evgeniy <epchelinov@gmail.com>
2021-09-09 20:05:53 +1000

    feat(тест): шум через бг гифку

--- src/scripts/app/core/noise.ts
+++ src/scripts/app/core/noise.ts
@@ -1,26 +1,27 @@
-import Grained from "@/scripts/_lib/Grained";
+// import Grained from "@/scripts/_lib/Grained";
 
-const options = {
-  animate: true,
-  patternWidth: 145,
-  patternHeight: 118,
-  grainOpacity: 0.04,
-  grainDensity: 1,
-  grainWidth: 1,
-  grainHeight: 1,
-};
+// const options = {
+//   animate: true,
+//   patternWidth: 145,
+//   patternHeight: 118,
+//   grainOpacity: 0.04,
+//   grainDensity: 1,
+//   grainWidth: 1,
+//   grainHeight: 1,
+// };
 
 export const addNoise = () => {
   const elms = [
     document.body,
     document.querySelector(".page-menu"),
     document.querySelector(".page-footer"),
-    document.querySelector(".footer-redirect")
+    document.querySelector(".footer-redirect"),
   ];
 
   elms.forEach((elm) => {
     if (elm) {
-      new Grained(elm, options);
+      // new Grained(elm, options);
+      elm.classList.add("bg-noise");
     }
   });
 };
