b45cf9360c171e630880ec2af9164cb77e2cc61f
Evgeniy <epchelinov@gmail.com>
2021-09-21 17:54:10 +1000

    feat(APP): добавился пересчет APP.isDesktop при resize

--- src/scripts/app/APP.ts
+++ src/scripts/app/APP.ts
@@ -1,12 +1,17 @@
-import detectIsTouchScreen from '@/scripts/utils/detectIsTouchScreen';
-import getOS from '@/scripts/utils/getOS';
-import { isDesktop } from '@/scripts/utils/mediaQueryEvent';
+import detectIsTouchScreen from "@/scripts/utils/detectIsTouchScreen";
+import getOS from "@/scripts/utils/getOS";
+import { getIsDesktop, isDesktop } from "@/scripts/utils/mediaQueryEvent";
+import { resizeObserver } from "../utils/resizeObserver";
 
 const APP: Window["APP"] = {
   os: getOS(),
   isDesktop,
   scrollbar: undefined,
-  isTouchScreen: detectIsTouchScreen()
+  isTouchScreen: detectIsTouchScreen(),
 };
+
+resizeObserver(document.documentElement, () => {
+  APP.isDesktop = getIsDesktop();
+});
 
 export default APP;
--- src/scripts/utils/mediaQueryEvent.ts
+++ src/scripts/utils/mediaQueryEvent.ts
@@ -3,6 +3,8 @@ import { log } from "./log";
 export const smallScreen = window.matchMedia("(max-width: 60em)");
 
 export const isDesktop = !window.matchMedia("(max-width: 60em)").matches;
+export const getIsDesktop = () =>
+  !window.matchMedia("(max-width: 60em)").matches;
 
 export const mediaQueryEvent = (
   cbMobile: () => void,
