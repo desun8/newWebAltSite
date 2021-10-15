42f2bee2470d29a9d0880f5375ebfb926d239c19
Evgeniy <epchelinov@gmail.com>
2021-10-15 18:31:11 +1000

    build

---
+++ dist/assets/recruitAnimation.2e08fa25.js
@@ -0,0 +1,1 @@
+import{e,g as r}from"./vendor.cfab4164.js";const t=()=>{const t=document.querySelector(".page-about .subscribe-banner-block");if(t){const o=t.querySelector(".js-banner");e.create({trigger:o,animation:r.to(o,{x:0,duration:.35}),start:"center 80%",once:!0})}};export{t as recruitAnimation};
