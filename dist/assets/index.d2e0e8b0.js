8d62e686c382387f8f4c2317334a8224e04edbae
Evgeniy <epchelinov@gmail.com>
2021-11-01 19:15:59 +1000

    build

--- dist/assets/index.d2e0e8b0.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./root.8c33ef14.js";const o=()=>{const o=document.querySelectorAll(".btn-play");o.length&&e((()=>import("./reviewVideo.0c502bfe.js")),["/assets/reviewVideo.0c502bfe.js","/assets/reviewVideo.0fc3c25c.css","/assets/vendor.5d6f67ed.js","/assets/root.8c33ef14.js","/assets/root.bde5c86c.css"]).then((({reviewVideo:e})=>{o.forEach((o=>{e(o)}))}))};export{o as w};
