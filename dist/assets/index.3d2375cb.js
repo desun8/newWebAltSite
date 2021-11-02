91a11677197b1d62ccaf147acf5b20617ec64dad
Evgeniy <epchelinov@gmail.com>
2021-11-02 16:56:37 +1000

    build

--- dist/assets/index.3d2375cb.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./root.2dc63a02.js";const o=()=>{const o=document.querySelectorAll(".btn-play");o.length&&e((()=>import("./reviewVideo.7705d656.js")),["/assets/reviewVideo.7705d656.js","/assets/reviewVideo.0fc3c25c.css","/assets/vendor.5d6f67ed.js","/assets/root.2dc63a02.js","/assets/root.871d513e.css"]).then((({reviewVideo:e})=>{o.forEach((o=>{e(o)}))}))};export{o as w};
