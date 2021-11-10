64efd4fb8804e92118c00a53a0f0a040223c5b46
Evgeniy <epchelinov@gmail.com>
2021-11-10 16:53:07 +1000

    build

--- dist/assets/blogOnScrollAnimation.8b2ff8f6.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD:dist/assets/blogOnScrollAnimation.8b2ff8f6.js
-import{d as o,g as e}from"./vendor.d00778cb.js";const t=o(Array.from(document.querySelectorAll(".blog-deco-top__item"))),r=document.querySelector(".blog-deco-top__cross");export default o=>{t&&t.length>0&&r&&(o=>{e.to([t,r],{y:(e,t)=>t===r?-1*o:o/(.55*(e+1))*-1,duration:.2,ease:"none"})})(o)};
-=======
-import{W as o,g as e}from"./vendor.5e96a560.js";const t=o(Array.from(document.querySelectorAll(".blog-deco-top__item"))),r=document.querySelector(".blog-deco-top__cross");export default o=>{t&&t.length>0&&r&&(o=>{e.to([t,r],{y:(e,t)=>t===r?-1*o:o/(.55*(e+1))*-1,duration:.2,ease:"none"})})(o)};
->>>>>>> prod:dist/assets/blogOnScrollAnimation.26c02954.js
