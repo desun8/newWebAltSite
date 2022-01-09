eab0b95cad80d1072cc2454b94ffcf2967ad93f4
Evgeniy Pchelinov <epchelinov@gmail.com>
2022-01-09 22:11:34 +1000

    feat: добавление широкого размера лайтбокса

--- src/scripts/components/lightbox/lightbox.ts
+++ src/scripts/components/lightbox/lightbox.ts
@@ -6,6 +6,8 @@ import "@/styles/components/lightbox.scss";
 
 export const lightbox = (elms: HTMLElement[]) => {
   elms.forEach((elm) => {
+    const isWideLightbox = elm.classList.contains("js-lightbox--wide");
+
     const img = elm.classList.contains("js-lightbox")
       ? elm.children[0].outerHTML
       : elm.outerHTML;
@@ -20,12 +22,22 @@ export const lightbox = (elms: HTMLElement[]) => {
 
         disableScroll(instance.element() as HTMLElement, APP.scrollbar);
 
+        if (isWideLightbox) {
+          document.body.classList.add("lightbox-wide");
+        }
+
         return true;
       },
       onClose: (instance) => {
         document.onkeydown = null;
 
         enableScroll(instance.element() as HTMLElement, APP.scrollbar);
+
+        if (isWideLightbox) {
+          setTimeout(() => {
+            document.body.classList.remove("lightbox-wide");
+          }, 500);
+        }
         return true;
       },
     });
